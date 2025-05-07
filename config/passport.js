7480a1eb7392fc51b6e7def4437bbeba5ddabc2c
Sujan Gautam <sujaan1919@gmail.com>
2025-05-07 17:17:51 -0500

    Update passport.js

--- config/passport.js
+++ config/passport.js
@@ -8,7 +8,7 @@ module.exports = function configurePassport() {
       {
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: `${process.env.API_URL}/api/auth/google/callback`,
+        callbackURL: `${process.env.API_URL}/auth/google/callback`,
       },
       async (accessToken, refreshToken, profile, done) => {
         try {
