d20aa8fbd2d26c2aa2aa2393dcd0c62c97903315
Sujan Gautam <sujaan1919@gmail.com>
2025-05-20 18:03:10 -0500

    Update passport.js

--- config/passport.js
+++ config/passport.js
@@ -15,25 +15,48 @@ module.exports = function configurePassport() {
           let user = await User.findOne({ googleId: profile.id });
 
           if (!user) {
-            user = await User.findOne({ email: profile.emails[0].value });
+            const email = profile.emails[0].value;
+            user = await User.findOne({ email });
 
             if (user) {
+              // Link Google account to existing user
               user.googleId = profile.id;
+              user.firstname = user.firstname || profile.name.givenName || 'User';
+              user.lastname = user.lastname || profile.name.familyName || '';
+              user.avatar = user.avatar || profile.photos[0].value;
+              user.lastLogin = new Date();
               await user.save();
             } else {
+              // Create new user with unique username
+              let username = profile.displayName.replace(/\s/g, '').toLowerCase();
+              const usernameExists = await User.findOne({ username });
+              if (usernameExists) {
+                // Append random string to avoid duplicates
+                username = `${username}_${Math.random().toString(36).substr(2, 5)}`;
+              }
+
               user = await User.create({
                 googleId: profile.id,
-                email: profile.emails[0].value,
-                username: profile.displayName.replace(/\s/g, '').toLowerCase(),
+                email,
+                username,
                 firstname: profile.name.givenName || 'User',
                 lastname: profile.name.familyName || '',
                 avatar: profile.photos[0].value,
+                lastLogin: new Date(),
               });
             }
+          } else {
+            // Update existing user
+            user.firstname = user.firstname || profile.name.givenName || 'User';
+            user.lastname = user.lastname || profile.name.familyName || '';
+            user.avatar = user.avatar || profile.photos[0].value;
+            user.lastLogin = new Date();
+            await user.save();
           }
 
           return done(null, user);
         } catch (error) {
+          console.error('Google Strategy error:', error);
           return done(error, null);
         }
       }
@@ -49,6 +72,7 @@ module.exports = function configurePassport() {
       const user = await User.findById(id);
       done(null, user);
     } catch (error) {
+      console.error('Deserialize error:', error);
       done(error, null);
     }
   });
